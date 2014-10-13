b1fa9799c576e29244ef6bb629868a0e4ce30f85
Fnyst <fnystrom73@gmail.com>
2014-10-13 20:40:39 +0200

    First

---
+++ ConsoleApplication1/ConsoleApplication1/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Car
+    {
+            public string color;
+            public string brand;
+            public float length;
+            public float weight;
+            protected int yearCreated;   
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.color = "Blue";
+            myCar.brand = "Volvo";
+            myCar.length = 2.5f;
+            myCar.weight = 2.15F;
+            
+
+            Console.WriteLine("My Car is a {0} {1} and it is {2} meter long and weigh {3} ton", myCar.color,myCar.brand,myCar.length,myCar.weight);
+            Console.WriteLine("My Car is made in {0} ", );
+            Console.ReadKey();
+
+        }
+    }
+}
